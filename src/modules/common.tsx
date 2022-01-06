062792692c5e48e1eabbf186e821ce286e16b95d
Lukas Zirngibl <lukas.e.zirngibl@gmail.com>
2022-01-06 20:36:35 +0100

    minor change

--- src/modules/common.tsx
+++ src/modules/common.tsx
@@ -5,12 +5,12 @@ import { MOBILE_BREAKPOINT } from '../constants'
 // DO NOT EDIT
 export const ModuleWrapper = styled.div`
   height: 600px;
-  margin-bottom: 32px;
+
   overflow: hidden;
 
   @media (min-width: ${MOBILE_BREAKPOINT}) {
     width: 1200px;
-    margin: 0 auto;
+    margin: 0 auto 32px 0;
   }
 
   @media (max-width: ${MOBILE_BREAKPOINT}) {
