c9b801ef4d4d36dc2c372de8741cf9db5631d16c
ferjus <fernando@foronered.com>
2023-05-17 10:03:40 +0200

    message

---
+++ src/modules/Module2.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import styled from 'styled-components'
+import { MOBILE_BREAKPOINT } from '../constants'
+import { ModuleWrapper } from './common'
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: blue;
+  justify-content: space-evenly;
+`
+
+const RedBox = styled.div`
+  width: 200px;
+  height: 200px;
+
+  @media (min-width: ${MOBILE_BREAKPOINT}) {
+    background-color: red;
+  }
+`
+
+export const Module2 = () => {
+  return (
+    <ModuleWrapper>
+      <Container>
+        <RedBox />
+        <RedBox />
+        <RedBox />
+      </Container>
+    </ModuleWrapper>
+  )
+}
